ac002cdd1d1d1449d7901343a9e0fbafb3f5e7dc
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-01-31 18:33:36 -0200

    heroku config

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -18,6 +18,20 @@ function createDBConnection() {
 			database : 'nodejs_sandbox_test'
 		});
 	}
+
+	if(process.env.NODE_ENV == 'production') {
+		var connectionURL = process.env.CLEARDB_DATABASE_URL;
+		var groups = connectionURL.match(/mysql:\/\/(.*):(.*)@(.*)\/(.*)\?reconnect=true/);
+
+		//mysql://bc4d94da7389fa:d13c18bd@us-cdbr-iron-east-04.cleardb.net/heroku_5da476e7893418a?reconnect=true
+
+		return mysql.createConnection({
+			host : groups[3],
+			user : groups[1],
+			password : groups[2],
+			database : groups[4]
+		});
+	}
 }
 
 module.exports = function() {
