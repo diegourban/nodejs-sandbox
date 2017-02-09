d189cd01782a51f8627cf85bbaad72cc77dde3cc
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-02-08 22:56:00 -0200

    basic functions

--- payfast/README.md
+++ payfast/README.md
@@ -6,6 +6,26 @@
 
 ## Installation
 * `npm install`
+* ` create database payfast;`
+* `CREATE TABLE `pagamentos` (
+        `id` int(11) NOT NULL AUTO_INCREMENT,
+       `forma_de_pagamento` varchar(255) NOT NULL,
+       `valor` decimal(10,2) NOT NULL,
+       `moeda` varchar(3) NOT NULL,
+       `status` varchar(255) NOT NULL,
+       `data` DATE,
+       `descricao` text,
+        PRIMARY KEY (id)
+       );`
 
 ## Usage
 * `nodemon index.js` - to start the server
+
+`curl http://localhost:3000/pagamentos -X POST -v -H "Content-type: application/json" -d '{
+  "forma_de_pagamento" : "payfast",
+  "valor" : 10,
+  "moeda" : "BRL",
+  "descricao" : "criando um pagamento"
+}'; echo`
+
+`curl http://localhost:3000/pagamentos -X POST -v -H "Content-type: application/json" -d @pagamento_sample.json; echo`
--- payfast/controllers/pagamentos.js
+++ payfast/controllers/pagamentos.js
@@ -5,4 +5,35 @@ module.exports = function(app) {
     res.send('OK.');
   });
 
+  app.post('/pagamentos', function(req, res) {
+    req.assert('forma_de_pagamento', 'Forma de pagamento é obrigatório').notEmpty();
+    req.assert('valor', 'Valor é obrigatório e deve ser um decimal').notEmpty().isFloat();
+    var errors = req.validationErrors();
+    if(errors) {
+      console.log('Erros de validação');
+      res.status(400).send(errors);
+      return;
+    }
+
+    var pagamento = req.body;
+    console.log('Processando uma requisição de um novo pagamento');
+
+    pagamento.status = 'CRIADO';
+    pagamento.data = new Date();
+
+    var connection = app.persistence.connectionFactory();
+    var paymentDao = new app.persistence.PaymentDAO(connection);
+
+    paymentDao.save(pagamento, function(err, result) {
+      if(err) {
+        console.log('Erro ao inserir no banco: ' + err);
+        res.status(500).send(err);
+      } else {
+        console.log('Pagamento criado');
+        res.location('/pagamentos/' + result.insertId);
+        res.status(201).json(pagamento);
+      }
+    });
+  });
+
 }
